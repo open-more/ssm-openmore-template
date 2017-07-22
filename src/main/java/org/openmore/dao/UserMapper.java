b9b9d83479c5c830ace25c0a2258d32ab548d497
MichaelTang <realmichael@126.com>
2017-07-22 22:46:04 +0800

    添加单元测试

--- src/main/java/org/openmore/dao/UserMapper.java
+++ src/main/java/org/openmore/dao/UserMapper.java
@@ -1,12 +1,7 @@
 package org.openmore.dao;
 
-import org.apache.ibatis.annotations.Param;
 import org.openmore.entity.User;
 import tk.mybatis.mapper.common.Mapper;
 
-/**
- * Created by michael on 2017/6/16.
- */
 public interface UserMapper extends Mapper<User> {
-    User getUserById(@Param("uid") long uid);
 }
